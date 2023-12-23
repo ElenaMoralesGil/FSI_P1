99d00eb23a11c77eb6218abaa63c5d6f6531d3b6
NelsonCC <nelsoncc1812@gmail.com>
2023-12-23 00:06:35 +0000

    Added comments

--- practica2/index.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-usecols=["image","MEL","NV","BCC","AK","BKL","DF","VASC","SCC","UNK"]
-csv = pd.read_csv(".dataset/ISIC_2019_Training_Metadata.csv", index_col="image", usecols=usecols)
-
-print(csv.head())
